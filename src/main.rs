efd3981d51e9858de89a2533bd8903da4db0a9d3
Robert <45889425+robpl4788@users.noreply.github.com>
2026-06-25 16:30:30 +0800

    ai makes it a bit prettier

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,18 @@
 use cortex_m::prelude::_embedded_hal_adc_OneShot;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::pwm;
 use panic_probe as _;
 use rp235x_hal::adc::AdcPin;
 use rp235x_hal::clocks::init_clocks_and_plls;
-use rp235x_hal::gpio::Pins;
 use rp235x_hal::pwm::Slices;
-use rp235x_hal::{self as hal, Adc, Timer, entry};
+use rp235x_hal::{self as hal, Adc, entry};
 use rp235x_hal::{Clock, pac};
 
 mod drivers;
 pub mod utils;
 mod v2_control_board;
 
-use drivers::{bldc, rgb_led};
+use v2_control_board::FireAntBoard;
 
 /// Tell the Boot ROM about our application
 #[unsafe(link_section = ".start_block")]
@@ -56,80 +54,15 @@ fn main() -> ! {
     );
 
     let pwm = Slices::new(pac.PWM, &mut pac.RESETS);
-
-    // Create the RGB LED driver
-    let mut red_green_slice = pwm.pwm0;
-    let mut blue_lphase_slice = pwm.pwm1;
-
-    let led_top = 65535;
-
-    red_green_slice.set_top(led_top);
-    red_green_slice.enable();
-
-    blue_lphase_slice.set_top(led_top);
-    blue_lphase_slice.enable();
-
-    let mut red_channel: v2_control_board::RGBRed = red_green_slice.channel_a;
-    let mut green_channel: v2_control_board::RGBGreen = red_green_slice.channel_b;
-    let mut blue_channel: v2_control_board::RGBBlue = blue_lphase_slice.channel_a;
-
-    let _blue_pin = blue_channel.output_to(pins.gpio18);
-    let _green_pin = green_channel.output_to(pins.gpio17);
-    let _red_pin = red_channel.output_to(pins.gpio16);
-
-    let mut rgb: v2_control_board::RGBLed =
-        rgb_led::RGBLed::new(red_channel, green_channel, blue_channel);
-
-    let mut lowc_lowb_slice = pwm.pwm5;
-    let mut lowa_highc_slice = pwm.pwm6;
-    let mut highb_highc_slice = pwm.pwm7;
-
-    let bldc_top = 65535;
-
-    lowc_lowb_slice.set_top(bldc_top);
-    lowc_lowb_slice.enable();
-
-    lowa_highc_slice.set_top(bldc_top);
-    lowa_highc_slice.enable();
-
-    highb_highc_slice.set_top(bldc_top);
-    highb_highc_slice.enable();
-
-    let mut lowc_channel = lowc_lowb_slice.channel_a;
-    let mut lowb_channel = lowc_lowb_slice.channel_b;
-    let mut lowa_channel = lowa_highc_slice.channel_a;
-    let mut highc_channel = lowa_highc_slice.channel_b;
-    let mut highb_channel = highb_highc_slice.channel_a;
-    let mut higha_channel = highb_highc_slice.channel_b;
-
-    let _lowc_pin = lowc_channel.output_to(pins.gpio10);
-    let _lowb_pin = lowb_channel.output_to(pins.gpio11);
-    let _lowa_pin = lowa_channel.output_to(pins.gpio12);
-    let _highc_pin = highc_channel.output_to(pins.gpio13);
-    let _highb_pin = highb_channel.output_to(pins.gpio14);
-    let _higha_pin = higha_channel.output_to(pins.gpio15);
-
-    let mut bldc: v2_control_board::BLDC = bldc::BLDC::new(
-        lowa_channel,
-        lowb_channel,
-        lowc_channel,
-        higha_channel,
-        highb_channel,
-        highc_channel,
-    );
-
-    let mut timer = Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+    let (mut board, gpio29) = FireAntBoard::new(pwm, pins);
 
     let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
-    let mut adc_pin_0 = AdcPin::new(pins.gpio29.into_floating_input()).unwrap();
+    let mut adc_pin_0 = AdcPin::new(gpio29.into_floating_input()).unwrap();
     adc.free_running(&adc_pin_0);
 
-    let mut last_change = timer.get_counter().ticks();
-    rgb.green();
+    board.rgb.green();
     loop {
-        // info!("full on!");
-        bldc.progress();
-        // bldc.disable();
+        board.bldc.progress();
         let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
         info!("ADC counts: {}", pin_adc_counts);
         delay.delay_ms(10);
--- src/v2_control_board.rs
+++ src/v2_control_board.rs
@@ -1,4 +1,8 @@
-use rp235x_hal::pwm::{A, B, Channel, FreeRunning, Pwm0, Pwm1, Pwm5, Pwm6, Pwm7, Slice};
+use crate::drivers::{bldc, rgb_led};
+use rp235x_hal::gpio::{FunctionNull, Pin, Pins, PullDown, bank0::Gpio29};
+use rp235x_hal::pwm::{
+    A, B, Channel, FreeRunning, Pwm0, Pwm1, Pwm5, Pwm6, Pwm7, Slice, SliceId, Slices,
+};
 
 pub type RGBRed = Channel<Slice<Pwm0, FreeRunning>, A>;
 pub type RGBGreen = Channel<Slice<Pwm0, FreeRunning>, B>;
@@ -16,3 +20,71 @@ pub type BLDCHighA = Channel<Slice<Pwm7, FreeRunning>, B>;
 
 pub type BLDC =
     crate::drivers::bldc::BLDC<BLDCLowA, BLDCLowB, BLDCLowC, BLDCHighA, BLDCHighB, BLDCHighC>;
+
+const LED_TOP: u16 = 65535;
+const BLDC_TOP: u16 = 7499;
+
+pub struct FireAntBoard {
+    pub rgb: RGBLed,
+    pub bldc: BLDC,
+}
+
+impl FireAntBoard {
+    pub fn new(pwm: Slices, mut pins: Pins) -> (Self, Pin<Gpio29, FunctionNull, PullDown>) {
+        fn configure_slice<T: SliceId>(slice: &mut Slice<T, FreeRunning>, top: u16) {
+            slice.set_top(top);
+            slice.enable();
+        }
+
+        let gpio29 = pins.gpio29;
+
+        let mut red_green_slice = pwm.pwm0;
+        let mut blue_lphase_slice = pwm.pwm1;
+
+        configure_slice(&mut red_green_slice, LED_TOP);
+        configure_slice(&mut blue_lphase_slice, LED_TOP);
+
+        let mut red_channel: RGBRed = red_green_slice.channel_a;
+        let mut green_channel: RGBGreen = red_green_slice.channel_b;
+        let mut blue_channel: RGBBlue = blue_lphase_slice.channel_a;
+
+        let _ = blue_channel.output_to(pins.gpio18);
+        let _ = green_channel.output_to(pins.gpio17);
+        let _ = red_channel.output_to(pins.gpio16);
+
+        let rgb = rgb_led::RGBLed::new(red_channel, green_channel, blue_channel);
+
+        let mut lowc_lowb_slice = pwm.pwm5;
+        let mut lowa_highc_slice = pwm.pwm6;
+        let mut highb_highc_slice = pwm.pwm7;
+
+        configure_slice(&mut lowc_lowb_slice, BLDC_TOP);
+        configure_slice(&mut lowa_highc_slice, BLDC_TOP);
+        configure_slice(&mut highb_highc_slice, BLDC_TOP);
+
+        let mut lowc_channel = lowc_lowb_slice.channel_a;
+        let mut lowb_channel = lowc_lowb_slice.channel_b;
+        let mut lowa_channel = lowa_highc_slice.channel_a;
+        let mut highc_channel = lowa_highc_slice.channel_b;
+        let mut highb_channel = highb_highc_slice.channel_a;
+        let mut higha_channel = highb_highc_slice.channel_b;
+
+        let _ = lowc_channel.output_to(pins.gpio10);
+        let _ = lowb_channel.output_to(pins.gpio11);
+        let _ = lowa_channel.output_to(pins.gpio12);
+        let _ = highc_channel.output_to(pins.gpio13);
+        let _ = highb_channel.output_to(pins.gpio14);
+        let _ = higha_channel.output_to(pins.gpio15);
+
+        let bldc = bldc::BLDC::new(
+            lowa_channel,
+            lowb_channel,
+            lowc_channel,
+            higha_channel,
+            highb_channel,
+            highc_channel,
+        );
+
+        (Self { rgb, bldc }, gpio29)
+    }
+}
